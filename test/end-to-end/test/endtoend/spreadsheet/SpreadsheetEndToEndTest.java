990d2fce584c23f69a01274d1e9b255c8a416296
Vincent Tencé <vtence@gmail.com>
2009-11-15 22:57:52 -0500

    Foundation for end-to-end tests

---
+++ test/end-to-end/test/endtoend/spreadsheet/SpreadsheetEndToEndTest.java
@@ -0,0 +1,12 @@
+package test.endtoend.spreadsheet;
+
+import org.junit.Test;
+
+public class SpreadsheetEndToEndTest {
+
+    @Test public void
+    entersNothingAndSeesAnEmptySpreadsheet() {
+        
+    }
+
+}
